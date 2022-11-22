be3d3f5fad3ad8aab6880998646412659a8084b8
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-22 08:17:31 +0100

    + kommentare

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,6 +1,6 @@
 package com.cc.java;
 
-public class App {
+public class App {                                                  // Klasse muss wie die Datei benannt sein
     public static void main( String[] args ) {
        System.out.println( "Hello, Test World!" ); 
     }
