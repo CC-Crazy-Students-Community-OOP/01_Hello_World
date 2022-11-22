6790d7bcf0d48c9c528e1c3eeb53a2cbf352e965
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-22 08:36:24 +0100

    +kommentare

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,7 +1,7 @@
 package com.cc.java;
 
-public class App {                                                  // Klasse muss wie die Datei benannt sein
-    public static void main( String[] args ) {
-       System.out.println( "Hello, Test World!" ); 
+public class App {                                              // Klasse muss wie die Datei benannt sein
+    public static void main( String[] args ) {                  // Methode (Funktion)  die main Methode ist Pflicht, Entry Point (Einstiegspunkt)
+        System.out.println( "Hello, Test World!" );             // Call (Aufruf) einer definierten Methode (println)
     }
 }
